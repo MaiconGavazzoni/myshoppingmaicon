d6385a4b97664caff27b1ec5949fe4048f66c2cd
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2022-07-25 21:46:20 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+
+
+import { Routes } from './src/routes';
+import theme from './src/theme';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+
+    <ThemeProvider theme={theme}>
+      <StatusBar style="light" />
+      <Routes />
+    </ThemeProvider>
+  );
+}
+
